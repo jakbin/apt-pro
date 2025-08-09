c0758c7c862ed488e916c3d6886bcbbddc5d03d4
jakbin <jakbin4747@gmail.com>
2025-08-09 17:50:35 +0530

    bump version to 1.1.2 and update module exports

--- apt_pro/__init__.py
+++ apt_pro/__init__.py
@@ -1,1 +1,26 @@
-__version__ = "1.1.1"
+from apt_pro.main import (
+    mylist,
+    upgradable_list,
+    update_apt,
+    add_pkg,
+    remove_pkg,
+    upgrade_pkg,
+    upgrade_pkg_regex,
+    show_dependencies,
+    upgrade_apt_pro
+)
+
+__version__ = "1.1.2"
+
+__all__ = [
+    "__version__",
+    "mylist",
+    "upgradable_list",
+    "update_apt",
+    "add_pkg",
+    "remove_pkg",
+    "upgrade_pkg",
+    "upgrade_pkg_regex",
+    "show_dependencies",
+    "upgrade_apt_pro"
+]
